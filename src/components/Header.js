98d150fe30d14a95d6c90dfc8b8c6f393c9c4ebb
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-03-01 22:18:11 +0600

    complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,43 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import tickImage from "../assets/images/double-tick.png";
 import noteImage from "../assets/images/notes.png";
 import plusImage from "../assets/images/plus.png";
+import { added, allcompleted, clearcompleted } from "../redux/todos/actions";
 
 export default function Header() {
+    const dispatch = useDispatch()
+    const [input, setInput] = useState('')
+
+    const handleInput = (e) => {
+        setInput(e.target.value)
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(added(input))
+        setInput('')
+    }
+
+    const completeAll = () => {
+        dispatch(allcompleted())
+    }
+
+    const allClear = () => {
+        dispatch(clearcompleted())
+    }
+
     return (
         <div>
-            <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+            <form
+                className="flex items-center bg-gray-100 px-4 py-4 rounded-md" onSubmit={submitHandler}>
                 <img src={noteImage} className="w-6 h-6" alt="Add todo" />
                 <input
                     type="text"
                     placeholder="Type your todo"
                     className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+                    value={input}
+                    onChange={handleInput}
                 />
                 <button
                     type="submit"
@@ -19,11 +46,11 @@ export default function Header() {
             </form>
 
             <ul className="flex justify-between my-4 text-xs text-gray-500">
-                <li className="flex space-x-1 cursor-pointer">
+                <li onClick={completeAll} className="flex space-x-1 cursor-pointer">
                     <img className="w-4 h-4" src={tickImage} alt="Complete" />
                     <span>Complete All Tasks</span>
                 </li>
-                <li className="cursor-pointer">Clear completed</li>
+                <li onClick={allClear} className="cursor-pointer">Clear completed</li>
             </ul>
         </div>
     );
--- src/redux/todos/reducer.js
+++ src/redux/todos/reducer.js
@@ -2,9 +2,9 @@ import { ADDED, ALLCOMPLETED, CLEARCOMPLETED, COLORSELECTED, DELETED, TOGGLED } 
 import { initialState } from "./initialState";
 
 const nextTodoId = (todos) => {
-    const maxId = todos.reducer((maxId, todo) => Math.max(todo.id, maxId), -1)
-    return maxId + 1
-}
+    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1);
+    return maxId + 1;
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
@@ -12,7 +12,9 @@ const reducer = (state = initialState, action) => {
             return [
                 ...state,
                 {
-                    id: nextTodoId(state)
+                    id: nextTodoId(state),
+                    text: action.payload,
+                    completed: false
                 }
             ]
 
